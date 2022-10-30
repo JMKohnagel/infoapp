0207ff980f2719e7bc55d8dad5fe7eaf6b54af41
Jan Marten Kohnagel <jmkohnagel@gmail.com>
2022-10-30 05:01:21 +0100

    IA-18 use own struct with image

--- src/infoapp/news/mod.rs
+++ src/infoapp/news/mod.rs
@@ -1,15 +1,31 @@
-use eframe::{egui::{Align, CentralPanel, Context, Layout, ScrollArea, Separator, Ui, Hyperlink}};
-use rss::{Channel,Item};
-use std::{
-    error::Error, io::BufReader,
-};
+use eframe::egui::{Align, CentralPanel, Context, Hyperlink, Layout, ScrollArea, Separator, Ui};
+use egui_extras::RetainedImage;
+use rss::Channel;
+use std::{error::Error, fmt::Debug, io::BufReader};
 
 const RSS_URL: &str = "https://www.tagesschau.de/xml/rss2/";
+
+struct NewsItem {
+    pub title: String,
+    pub link: String,
+    pub description: String,
+    pub image: RetainedImage,
+}
+
+impl Debug for NewsItem {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("NewsItem")
+            .field("title", &self.title)
+            .field("link", &self.link)
+            .field("description", &self.description)
+            .finish()
+    }
+}
 
 #[derive(Debug)]
 pub struct News {
     rss_url: String,
-    items: Vec<Item>,
+    items: Vec<NewsItem>,
 }
 
 impl News {
@@ -25,7 +41,7 @@ impl News {
             self.render_top_bar(ui);
             ui.add_space(1.0);
             ui.add(Separator::default().horizontal());
-            self.render_items(ui)
+            self.render_items(ui, ctx);
         });
     }
 
@@ -38,8 +54,9 @@ impl News {
                 let refresh_button = ui.button("Refresh");
                 if refresh_button.clicked() {
                     if self.rss_url.is_empty() {
-                        ui.label("RSS url is not specified");   // TODO input for rss url
+                        ui.label("RSS url is not specified");
                     } else {
+                        self.items = Vec::new();
                         self.get_items();
                     }
                 }
@@ -47,28 +64,28 @@ impl News {
         });
     }
 
-    fn render_items(&self, ui: &mut Ui) {
+    fn render_items(&self, ui: &mut Ui, ctx: &Context) {
         ScrollArea::vertical().show(ui, |ui| {
             for item in &self.items {
                 ui.horizontal(|ui| {
-                    // TODO: add image
+                    ui.image(item.image.texture_id(ctx), item.image.size_vec2());
                     ui.vertical(|ui| {
-                        ui.label(item.title().unwrap_or("No title"));
-                        ui.label(item.description().unwrap_or("No description"));
-                        ui.with_layout(Layout::right_to_left(Align::LEFT), |ui| {
-                            ui.add(Hyperlink::from_label_and_url("read more ⤴", item.link().unwrap_or("No Link")));
+                        ui.label(&item.title);
+                        ui.label(&item.description);
+                        ui.with_layout(Layout::right_to_left(Align::BOTTOM), |ui| {
+                            ui.add(Hyperlink::from_label_and_url("read more ⤴", &item.link));
                         });
                         ui.add_space(1.0);
-                        ui.add(Separator::default().horizontal());
-                    })
+                    });
                 });
+                ui.add(Separator::default().horizontal());
             }
         });
     }
 
     fn get_channel(&self) -> Result<Channel, Box<dyn Error>> {
-        let content = ureq::get(&self.rss_url).call()?.into_string()?;
-        let channel = Channel::read_from(BufReader::new(content.as_bytes()))?;
+        let content = reqwest::blocking::get(&self.rss_url).unwrap().bytes().unwrap();
+        let channel = Channel::read_from(BufReader::new(&content[..]))?;
         Ok(channel)
     }
 
@@ -78,6 +95,32 @@ impl News {
             Ok(c) => c.into_items().to_vec(),
             Err(_) => Vec::new(),
         };
-        self.items = items;
+        items.iter().for_each(|item| {              // threading
+            let image = self.get_image(self.get_image_url(item.content()));
+            let news_item = NewsItem {
+                title: item.title().unwrap_or("No title").to_string(),
+                link: item.link().unwrap_or("No link").to_string(),
+                description: item.description().unwrap_or("No description").to_string(),
+                image,
+            };
+            self.items.push(news_item);
+        });
+    }
+
+    fn get_image(&self, url: &str) -> RetainedImage {       // TODO error handling
+        let image_bytes = reqwest::blocking::get(url).unwrap().bytes().unwrap();
+        RetainedImage::from_image_bytes("image", &image_bytes).unwrap()
+    }
+
+    fn get_image_url<'a>(&'a self, content: Option<&'a str>) -> &str {
+        let context_string = match content {
+            Some(c) => c,
+            None => todo!("default image"),
+        };
+        let re = regex::Regex::new(r#"<img src="([^"]+)""#).unwrap();
+        match re.captures(context_string) {
+            Some(caps) => caps.get(1).map_or("", |m| m.as_str()),
+            None => todo!("default image"),
+        }
     }
 }
