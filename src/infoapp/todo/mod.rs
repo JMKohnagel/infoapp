d01d195839e5eab8616142cc4359c1cbacf77ab6
JMKohnagel <jmkohnagel@gmail.com>
2022-11-16 21:15:43 +0100

    IA-37 search todos

--- src/infoapp/todo/mod.rs
+++ src/infoapp/todo/mod.rs
@@ -8,6 +8,7 @@ const LIST_NAME: &str = "test";
 pub struct Todo {
     items: Vec<TodoItem>,
     next_id: usize,
+    filter_value: String,
 }
 
 #[derive(Clone)]
@@ -37,6 +38,7 @@ impl Todo {
         Todo {
             items,
             next_id: last_id + 1,
+            filter_value: String::new(),
         }
     }
 
@@ -45,6 +47,7 @@ impl Todo {
             ui.heading(RichText::new("Todo").strong());
             ui.add_space(1.0);
             ui.add(Separator::default().horizontal());
+            ui.text_edit_singleline(&mut self.filter_value);
             ScrollArea::vertical().show(ui, |ui| {
                 self.render_items(ui);
                 if ui.button("+").on_hover_text("Add Item").clicked() {
@@ -59,18 +62,20 @@ impl Todo {
     fn render_items(&mut self, ui: &mut eframe::egui::Ui) {
         let mut items_to_delete: Vec<usize> = vec![];
         let mut save = false;
-        for item in self.items.iter_mut() {
+        let filtered_ids = filter_items(&self.items, &self.filter_value);
+        for id in filtered_ids.iter() {
+            let self_item = get_item_from_id(&mut self.items, id.clone()).unwrap();
             ui.horizontal(|ui| {
-                let checkbox = ui.checkbox(&mut item.done, "");
-                let input = ui.text_edit_singleline(&mut item.text);
-                if input.lost_focus() && !item.text.is_empty() {
+                let checkbox = ui.checkbox(&mut self_item.done, "");
+                let input = ui.text_edit_singleline(&mut self_item.text);
+                if input.lost_focus() && !self_item.text.is_empty() {
                     save = true;
                 }
-                if checkbox.clicked() && !item.text.is_empty() {
+                if checkbox.clicked() && !self_item.text.is_empty() {
                     save = true;
                 }
-                if ui.button("-").on_hover_text("Delete").clicked() {
-                    items_to_delete.push(item.id);
+                if ui.button("x").on_hover_text("Delete").clicked() {
+                    items_to_delete.push(self_item.id);
                 };
             });
         }
@@ -86,6 +91,23 @@ impl Todo {
         self.items.retain(|item| !ids.contains(&item.id));
         save_items(&self.items)
     }
+}
+
+fn get_item_from_id(items: &mut [TodoItem], id: usize) -> Option<&mut TodoItem> {
+    for item in items.iter_mut() {
+        if item.id == id {
+            return Some(item);
+        }
+    }
+    return None;
+}
+
+fn filter_items(items: &[TodoItem], filter_value: &str) -> Vec<usize> {
+    return items
+        .iter()
+        .filter(|item| item.text.to_lowercase().contains(filter_value.to_lowercase().as_str()))
+        .map(|item| item.id)
+        .collect();
 }
 
 // load items from file
